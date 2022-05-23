5b282587659c3d21de14bea970f174e5e62607f8
chikuh <101875414+chikuh@users.noreply.github.com>
2022-05-23 11:39:37 +0530

    Update Info.ts

--- src/commands/General/Info.ts
+++ src/commands/General/Info.ts
@@ -38,9 +38,9 @@ export default class Command extends BaseCommand {
             MessageType.video,
             Mimetype.gif,
             undefined,
-            `━━━❰ 🅨︎🅞︎🅣︎🅢︎🅤︎🅑︎🅐︎ ❱━━━\n\n🔮 *Groups: ${
+            `━━━❰ 🅧🅐🅒🅑🅞🅣 ❱━━━\n\n🔮 *Groups: ${
 				chats.length
-					}*\n\n🚦 *Uptime: ${uptime()}*\n\n🦆 *My cute people: ${users}*\n\n💢 *Banned Users: ${uban}*\n\n\n`,
+					}*\n\n🚦 *Uptime: ${uptime()}*\n\n🥺 *My sexy peoples: ${users}*\n\n😡 *Banned Users: ${uban}*\n\n\n`,
                     undefined
                 ).catch((reason: any) =>
             M.reply(`✖ An error occurred. Please try again later.`))	
