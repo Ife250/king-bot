87d738594bcad18490cbd2315afa537b96bb7a0e
chikuh <101875414+chikuh@users.noreply.github.com>
2022-05-23 11:25:17 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -17,14 +17,14 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '🍥 *Moderators* 🍥\n\n'
+        let text = '✨ *Moderators* ✨\n\n'
         filteredMap.forEach(
             (user, index) =>
-                (text += `#${index + 1}\n⚡ *Username: ${
+                (text += `#${index + 1}\n🎆 *Username: ${
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n⚡ Yotsuba ⚡ `
+        text += `\n🎆 𝚇𝙰𝙲-𝙱𝙾𝚃 🎆 `
         return void M.reply(text)
     }
 }
