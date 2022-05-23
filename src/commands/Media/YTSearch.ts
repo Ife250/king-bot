62eaaea35d07caa06f6da787d2ca525d6a0affe8
chikuh <101875414+chikuh@users.noreply.github.com>
2022-05-23 11:53:24 +0530

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -29,14 +29,14 @@ export default class Command extends BaseCommand {
                 videos[i].author.name
             }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
         }
-        M.reply('⚡ Searching...')
+        M.reply('🪄 Searching babe...')
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
                 quoted: M.WAMessage,
                 contextInfo: {
                     externalAdReply: {
                         title: `Search Term: ${term}`,
-                        body: `⚡ Yotsuba ⚡`,
+                        body: `🎆 𝚇𝙰𝙲-𝙱𝙾𝚃 🎆`,
                         mediaType: 2,
                         thumbnailUrl: videos[0].thumbnail,
                         mediaUrl: videos[0].url
