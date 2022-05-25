3f061c959635f9ce1466d720447f893005306d6b
Kingsley <100943735+Ife250@users.noreply.github.com>
2022-05-25 14:56:20 +0200

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -26,7 +26,7 @@ export default class EventHandler {
 					? `Goodbye *@${
 							event.participants[0].split("@")[0]
 					  }* 👋🏻, we're probably not gonna miss you.`
-					: `Ara Ara, looks like *@${
+					: `wow, looks like *@${
 							event.participants[0].split("@")[0]
 					  }* got ${this.client.util.capitalize(event.action)}d${
 							event.actor ? ` by @${event.actor.split("@")[0]}` : ""
