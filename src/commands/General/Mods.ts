bd70d99ce1df2f945faa0a8fda3ad79de8219310
Kingsley <100943735+Ife250@users.noreply.github.com>
2022-05-25 14:19:22 +0200

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -7,24 +7,24 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'mods',
-            description: "Displays the Moderators' contact info",
+            description: "Displays the legends/Moderators' contact info",
             category: 'general',
             usage: `${client.config.prefix}mods`,
-            aliases: ['moderators', 'mod', 'owner']
+            aliases: ['moderators', 'mod', 'owner', 'kings', 'legends']
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
         const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '✨ *Moderators* ✨\n\n'
+        let text = '✨ *legends* ✨\n\n'
         filteredMap.forEach(
             (user, index) =>
                 (text += `#${index + 1}\n🎆 *Username: ${
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\n🎆 𝚇𝙰𝙲-𝙱𝙾𝚃 🎆 `
+        text += `\n🎆 ꪶ࿋྄ིᤢꫂKíñgsléyꪶ࿋྄ིᤢꫂ 🎆 `
         return void M.reply(text)
     }
 }
